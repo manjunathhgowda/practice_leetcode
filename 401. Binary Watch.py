3efa8b8e7e88f850cecfd348884f29dc26eee2be
Manjunatha H B <manjunathhgowda1432@gmail.com>
2026-02-17 10:54:58 +0530

    New file

---
+++ 401. Binary Watch.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def readBinaryWatch(self, turnedOn):
+        result = []
+
+        for hour in range(12):
+            for minute in range(60):
+                if (bin(hour).count('1') + bin(minute).count('1')) == turnedOn:
+                    result.append("{}:{:02d}".format(hour, minute))
+
+        return result
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    print(sol.readBinaryWatch(1))
+    print(sol.readBinaryWatch(9))
