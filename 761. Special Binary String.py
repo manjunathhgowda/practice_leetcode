779f26b3294b6cd9b8a313e2c1670daad8bae1b6
Manjunatha H B <manjunathhgowda1432@gmail.com>
2026-02-21 00:15:19 +0530

    New File

---
+++ 761. Special Binary String.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def makeLargestSpecial(self, s):
+        count = 0
+        start = 0
+        parts = []
+
+        for i in range(len(s)):
+            if s[i] == '1':
+                count += 1
+            else:
+                count -= 1
+
+            if count == 0:
+                inner = self.makeLargestSpecial(s[start + 1:i])
+                parts.append("1" + inner + "0")
+                start = i + 1
+
+        parts.sort(reverse=True)
+        return "".join(parts)
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    print(sol.makeLargestSpecial("11011000"))  # 11100100
+    print(sol.makeLargestSpecial("10"))        # 10
